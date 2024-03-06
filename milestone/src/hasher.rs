2d485f478cdc068f61ad2686e583fa4c83513c80
John Guibas <jtguibas@Johns-MBP.monkeybrains.net>
2024-03-06 11:29:43 -0800

    perf: keccak precompile

--- milestone/src/hasher.rs
+++ milestone/src/hasher.rs
@@ -1,16 +1,22 @@
-use rs_merkle::Hasher;
-use sha3::{Digest, Keccak256};
+use rs_merkle::Hasher as MerkleHasher;
+use tiny_keccak::{Hasher, Keccak};
+
+pub fn keccak256(data: &[u8]) -> [u8; 32] {
+    let mut keccak256 = Keccak::v256();
+    keccak256.update(data);
+    let mut output = [0u8; 32];
+    keccak256.finalize(&mut output);
+    output
+}
 
 #[derive(Clone)]
 pub struct KeccakAlgorithm {}
 
-impl Hasher for KeccakAlgorithm {
+impl MerkleHasher for KeccakAlgorithm {
     type Hash = [u8; 32];
 
     fn hash(data: &[u8]) -> [u8; 32] {
-        let mut hasher = Keccak256::new();
-        hasher.update(data);
-        <[u8; 32]>::from(hasher.finalize())
+        keccak256(data) 
     }
 }
 
