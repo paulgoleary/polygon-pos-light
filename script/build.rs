f8c54073f6914d89e980cd252242795e1f05efd8
Uma Roy <uma.roy.us@gmail.com>
2024-03-07 16:59:29 -0800

    Added to build.rs

--- script/build.rs
+++ script/build.rs
@@ -1,5 +1,7 @@
 use sp1_helper::build_program;
 
 fn main() {
+    // This is a hack to make sure that the build script is re-run when the milestone dependency is changed.
+    println!("cargo:rerun-if-changed=../milestone");
     build_program("../program")
 }
